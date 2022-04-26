9252274af669c996d603171f73f647c1bae242d4
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2022-04-26 23:32:32 +0530

    Again Fucking Testing

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,7 +1,8 @@
 import { useRef , useContext , useState , useCallback } from "react"
 import { ScrollContext } from "../utils/scroll-observer"
 import "./Styles/Homepage.css"
-import BgImage from "../Assests/bg-Image.jpg"
+import BgImage from "../Assests/BG,Image.jpg"
+import DownArrow from "../Assests/downArrow.png"
 
 const Homepage: React.FC = () => {
 	const refCont =  useRef<HTMLDivElement>(null)
@@ -21,12 +22,15 @@ const Homepage: React.FC = () => {
 		<div className="hidden sm:flex sm:h-full sm:w-1/2 bg-[#070707]">
 		</div>
 	<div className="absolute top-1/4 left-4 sm:left-16 homeTitle">
-	<h1 className="text-white text-5xl sm:text-[5rem] md:text-[7rem]">ENVISION</h1>
+	<h1 className="text-white text-5xl sm:text-[5rem] md:text-[7rem] drop-shadow-2xl">ENVISION</h1>
 	<h2 className="text-[#E40031] sm:mt-6 text-4xl sm:text-[2.7rem] md:text-[4rem]"> ARTISTS </h2>
 	</div>	
 		<div className="h-full max-w-screen w-full sm:w-1/2 flex ">
 			{/* <div className="bg_image flex-grow-0 flex-shrink-0"></div> */}
 			<img className="bg_image object-cover w-full h-auto" src={BgImage} alt="" />
+		</div>
+		<div className="absolute bottom-2 left-[45%] sm:left-[47.8%]">
+			<img src={DownArrow} className="rotate-[270deg]" width={130/ 3}  height={65/3} alt="" />
 		</div>
 	 </section>
 	 </>
